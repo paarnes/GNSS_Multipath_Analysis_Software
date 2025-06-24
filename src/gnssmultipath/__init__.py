69fa0c34cb9afa7ca8d48291ace6f906c9651316
Per Helge Aarnes <per.helge.aarnes@gmail.com>
2025-06-24 12:57:14 +0000

    add version

--- src/gnssmultipath/__init__.py
+++ src/gnssmultipath/__init__.py
@@ -1,3 +1,13 @@
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError  # type: ignore
+
+try:
+    __version__ = version("gnssmultipath")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
 from .GNSS_MultipathAnalysis import GNSS_MultipathAnalysis
 from .readRinexObs import readRinexObs
 from .RinexNav import Rinex_v3_Reader
